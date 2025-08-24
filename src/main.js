c39349bbfe3daa0f05a67dc339d050b859713e4f
BrandonPilane <BrandonPilane@gmail.com>
2025-08-24 17:33:41 +0200

    feat: done

--- src/main.js
+++ src/main.js
@@ -3,13 +3,21 @@ import { readKeypresses } from './utils/keypress.js';
 
 let keypresses = [];
 
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('#app').innerHTML = `
+    <div style="font-size:2em;" >
+      Start typing to see your keypresses...
+      <aside style="font-size:1rem;" >Sorry mobile users, this is a desktop only app.</aside>
+    </div>`;
+});
+
 document.addEventListener('keydown', (event) => {
   readKeypresses(keypresses, event);
   console.log(keypresses);
 
   document.querySelector('#app').innerHTML = `
-    <div id="keypresses">
-      ${keypresses.map((key) => `<div>${key}</div>`).join('')}
+    <div id="container">
+      ${keypresses.map((key) => `<div class="key">${key}</div>`).join('')}
     </div>
   `;
 });
